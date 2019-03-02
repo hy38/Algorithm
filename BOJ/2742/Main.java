26d2147aa43ed43e47c4225534a898dea736cf0d
hy38 <sanghyun12101@gmail.com>
2019-03-02 16:50:29 +0900

    BOJ #2742: 기찍 N

---
+++ BOJ/2742/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Author: SangHyun Park (hy38)
+ * Date: 2019.03.02
+ * E-mail: sanghyun12101@gmail.com
+ *
+ * https://github.com/hy38/Algorithm
+ * https://www.acmicpc.net/problem/2742
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+
+        for (int i = T; i > 0; i--)
+            System.out.println(i);
+    }
+}
